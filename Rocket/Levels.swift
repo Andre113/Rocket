3caa3be640a5895c2f4df0cc5a34a86b462d0e76
wellpardim <wellingtons_10@hotmail.com>
2015-05-28 00:46:53 -0300

    core data

---
+++ Rocket/Levels.swift
@@ -0,0 +1,21 @@
+//
+//  Levels.swift
+//  Rocket
+//
+//  Created by Wellington Pardim Ferreira on 5/28/15.
+//  Copyright (c) 2015 Andre Lucas Ota. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Levels: NSManagedObject {
+
+    @NSManaged var level1: Bool
+    @NSManaged var level2: Bool
+    @NSManaged var level3: Bool
+    @NSManaged var level4: Bool
+    @NSManaged var level6: Bool
+    @NSManaged var level5: Bool
+
+}
